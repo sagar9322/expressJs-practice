202ce4357249d479c9e2a28c1479b20102d346ea
sagar9322 <sagarkasundra223@gmail.com>
2023-06-05 23:42:01 +0530

    Sequelize-first-commit

--- util/database.js
+++ util/database.js
@@ -1,10 +1,15 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Sagar@549412'
-});
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node-complete',
+//     password: 'Sagar@549412'
+// });
 
-module.exports = pool.promise();
+// module.exports = pool.promise();
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('node-complete', 'root', 'Sagar@549412', {dialect: 'mysql', host:'localhost'});
+
+module.exports = sequelize;
