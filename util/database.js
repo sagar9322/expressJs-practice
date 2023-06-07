7454d30a5ca643e221cdf14f77bfd89633677007
sagar9322 <sagarkasundra223@gmail.com>
2023-06-07 12:11:01 +0530

    sql-last

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'Sagar@549412', {
+const sequelize = new Sequelize('node-complete', 'root', '*******', {
   dialect: 'mysql',
   host: 'localhost'
 });
